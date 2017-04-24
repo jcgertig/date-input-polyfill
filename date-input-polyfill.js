8e8cae11f8261d66d7e7173d810c0780df61106f
Craig Morey <craigmorey@gmail.com>
2017-04-24 14:39:39 +0200

    stop polyfill initialising if there are no date inputs on the page or if the browser already supports a date picker.

--- date-input-polyfill.js
+++ date-input-polyfill.js
@@ -1,7 +1,22 @@
 import './date-input-polyfill.scss';
-import Input from './input.js';
 
 const addPickers = () => {
+  // Only initialise if there is an input with type="date" in the document
+  if (typeof document.querySelectorAll !== 'undefined' && !document.querySelectorAll('input[type="date"]').length) {
+    return;
+  }
+  // Only initialise if the current browser does not support type="date" natively
+  const isDateSupported = function () {
+    var i = document.createElement("input");
+    i.setAttribute("type", "date");
+    return i.type !== "text";
+  };
+  if (isDateSupported()) {
+    return;
+  }
+  // Only now do we need to load dependencies
+  const Input = require('./input.js').default;
+  
   Input.addPickerToOtherInputs();
   // Check if type="date" is supported.
   if(!Input.supportsDateInput()) {
